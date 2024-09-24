d416e87e20ed678969f341f325da54aa53442928
sanika mahajan <114577918+Sanika8767@users.noreply.github.com>
2024-09-24 04:53:19 -0700

    Add files via upload

---
+++ prac6.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('.product button').forEach(button => {
+    button.addEventListener('click', () => {
+        
+        console.log('Product added to cart!');
+    });
+});
